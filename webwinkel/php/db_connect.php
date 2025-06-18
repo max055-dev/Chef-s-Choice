8829c274f634e81bcd137aa1c3bedc9d9401e80d
max055-dev <maxterwel@gmail.com>
2025-06-18 10:26:10 +0200

    oude site verwijdert

--- webwinkel/php/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root"; // Standaard MySQL-gebruiker bij XAMPP
-$password = ""; // Standaard MySQL-wachtwoord is leeg bij XAMPP
-$database = "webwinkel"; // De naam van je geïmporteerde database
-
-// Maak een verbinding
-$conn = new mysqli($servername, $username, $password, $database);
